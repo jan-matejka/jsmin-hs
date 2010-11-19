d6eaddd0b994998beae52f5aa886225b6b1e2ef1
yac <yac@blesmrt.net>
2010-11-19 06:42:08 +0100

    fix refactored, though now it is just able to switch basic states and only if state does not span multiple lines. fix copyright fix licence removed c code, the useless test runners

--- jsmin.hs
+++ jsmin.hs
@@ -1,6 +1,5 @@
 {- jsmin.hs
-   2008-08-03
-Copyright (c) 2010 Jan Yac Matejka (yac@blesmrt.net)
+Copyright (c) 2010 Jan Yac Matejka (yac.blesmrt.net)
 Copyright (c) 2002 Douglas Crockford  (www.crockford.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -12,6 +11,8 @@ so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
+The Software shall be used for Good, not Evil.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -28,113 +29,109 @@ import System
 import IO
 import Ix
 import Char
--- import Control.Monad.State
+import Control.Monad.State
+import Maybe
 
+is_wsp :: Char -> Bool
 is_wsp x = elem x (map chr (range(0,9)++[11,12,127]++range(14,32)))
+
+cr_or_lf :: Char -> Bool
 cr_or_lf x = elem x "\n\r"
+
+is_alphanum :: Char -> Bool
 is_alphanum x = elem x (map chr (range(48,57)++range(65,90)++range(97,122)))
 
+wsp_keeper :: Char -> Bool
 wsp_keeper x
 	| is_alphanum x = True
 	| elem x "\\$_" = True
 	| True = False
 
+nl_pre_keeper :: Char -> Bool
 nl_pre_keeper x
 	| is_alphanum x = True
 	| elem x "\\$_}])+-\"'" = True
 	| True = False
 
+nl_post_keeper :: Char -> Bool
 nl_post_keeper x
 	| is_alphanum x = True
 	| elem x "\\$_{[(+-" = True
 	| True = False
 
+re_pre_keeper :: Char -> Bool
 re_pre_keeper x = elem x "(,=:[!&|?{};\n"
 
+consume :: String -> String
+consume xs = xs
 
-s_commentMultiEnd xs (y:[])
-	| y == '/' = s_start xs " "
-	| y == '*' = moar s_commentMultiEnd xs
-	| True = moar s_commentMulti xs
+ommit :: String -> String
+ommit xs = ""
 
-s_commentMulti xs [] = putStr "Error: JSMIN Unterminated comment."
-s_commentMulti xs (y:[])
-	| y == '*' = moar s_commentMultiEnd xs
-	| True = moar s_commentMulti xs
-
-s_commentInline xs (y:[])
-	| cr_or_lf y = s_start xs "\n"
-	| True = moar s_commentInline xs
-
-s_maybeCommentStart (xs) (y:[])
-	| y == '/' = moar s_commentInline xs
-	| y == '*' = moar s_commentMulti xs
-	| re_pre_keeper (last xs) = s_regexp (xs++"/") [y]
-	| True = s_start (xs++"/") [y]
+until' :: String -> (String -> String) ->  (String,String) -> PartialParserResult
+until' xs f (a,b)
+	| length xs > length a = (a,b,False)
+	| take (length xs) a == xs = (drop (length xs) a, b++(f xs), True)
+	| True = until' xs f (tail a, b++(f [head a]))
 
 
-s_maybeWSP xs ys 
-	| is_wsp(head ys) = moar s_maybeWSP xs
-	| wsp_keeper (head ys) = do
-		putStr (xs++[' '])
-		s_start [] ys
-	| True = s_start xs ys
+type PartialParserResult = (String,String,Bool)
+type PartialParser = (String,String) -> PartialParserResult 
+type ParseState = (String, String, PartialParser)
+--              buffer, result, parser
 
-s_maybeNL xs (y:[])
-	| cr_or_lf y || is_wsp y = moar s_maybeNL xs
-	| nl_post_keeper y = do
-		putStr (xs++"\n")
-		moar s_start [y]
-	| True = s_start xs [y]
+parse_code :: (String,String) -> PartialParserResult
+parse_code ([],b) = ([],b,True)
 
-s_maybeNL xs [] = do
-	putStr xs
+parse_code (a,b) =
+	case (head a) of
+		'"' ->  parse_string (tail a, b++"\"STR")
+		'\'' -> parse_string' (tail a, b++"'STR2")
+		'/' ->
+			case (take 2 a) of
+				"//" -> parse_comment_inline (a,b++"CM")
+				"/*" -> parse_comment_multiline (a,b++"CM2")
+				_ -> parse_regexp (tail a,b++"RE")
+		_ -> parse_code (tail a,b++[head a])
 
-s_start xs [] = putStr xs
-s_start xs ('/':[]) = moar s_maybeCommentStart xs
-s_start [] (y:[])
-	| is_wsp y || cr_or_lf y = moar s_start []
-	| True = moar s_start [y]
+parse_regexp = until' "/" consume
+parse_string' = until' "'" consume
+parse_string :: (String,String) -> PartialParserResult
+parse_string = until' "\"" consume
+parse_comment_inline = until' "\n" ommit
+parse_comment_multiline = until' "*/" ommit
 
-s_start xs (y:[])
-	| nl_pre_keeper (last xs) && cr_or_lf y = moar s_maybeNL xs
-	| wsp_keeper (last xs) && is_wsp y = moar s_maybeWSP xs
-	| is_wsp y || cr_or_lf y = moar s_start xs
-	| y == '\'' = moar s_string1 (xs++[y])
-	| y == '"' = moar s_string2 (xs++[y])
-	| True = do 
-		moar s_start (xs++[y])
-
-s_string1 xs ('\\':[]) = moar s_string1free (xs++"\\")
-s_string1 xs ('\'':[]) = moar s_start (xs++"'")
-s_string1 xs (y:[]) = moar s_string1 (xs++[y])
-s_string1 xs [] = putStrLn "Error: JSMIN unterminated string literal."
-
-s_string2 xs ('\\':[]) = moar s_string2free (xs++"\\")
-s_string2 xs ('"':[]) = moar s_start (xs++"\"")
-s_string2 xs (y:[]) = moar s_string2 (xs++[y])
-s_string2 xs [] = putStrLn "Error: JSMIN unterminated string literal."
-
-s_regexp xs ('\\':[]) = moar s_regexpfree (xs++"\\")
-s_regexp xs ('/':[]) = moar s_start (xs++"/")
-s_regexp xs (y:[]) = moar s_regexp (xs++[y])
-s_regexp xs [] = putStrLn "Error: JSMIN unterminated Regular Expression literal."
-
--- these are executed when escaping character is encountered to ignore next char if it was terminating w/o the escape
-s_string2free xs (y:[]) = moar s_string2 (xs++[y])
-s_string1free xs (y:[]) = moar s_string1 (xs++[y])
-s_regexpfree xs (y:[]) = moar s_regexp (xs++[y])
-
-moar state xs = do
-	iseof <- IO.isEOF
-	if iseof then state xs ""
+parse :: State ParseState ParseState
+parse = do
+	(a,b,f) <- get
+	let (a1,b1,finished) = f (a,b)
+	if finished
+		then return (a1,b1,parse_code)
 		else do
-			c <- getChar
-			state xs [c]
+			put (a1,b1,f)
+			return (a1,b1,f)
+
+parse2 (a,b,f) = do
+	let (a1,b1,flag) = f (a,b)
+	if flag
+		then (a1,b1,f)
+		else (a1,b1,parse_code)
+
+moar :: ParseState -> IO ()
+moar state = do
+--	let (a,b,f) = evalState (parse) state
+	let (a,b,f) = parse2 state
+	putStr b
+	iseof <- IO.isEOF
+	if iseof
+		then return ()
+		else do
+			l <- IO.getLine
+			moar (a++l++"\n","",f)
 
 main = do
 	args <- getArgs
-	if length(args) > 0
+	if not (null args)
 		then putStrLn ("// "++(head args))
 		else putStr ""
-	moar s_start [] 
+	moar ("","",parse_code)
